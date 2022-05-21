4d4a4a241bb2ccda7a3b1506bc2457d77711b807
qiuhao1470 <105741420+qiuhao1470@users.noreply.github.com>
2022-05-20 22:37:15 -0700

    Update SQLQuery_hw3.sql

--- SQLQuery_hw3.sql
+++ SQLQuery_hw3.sql
@@ -1,7 +1,7 @@
 --Use Northwind database. All questions are based on assumptions described by the Database Diagram sent to you yesterday. When inserting, make up info if necessary. Write query for each step. Do not use IDE. BE CAREFUL WHEN DELETING DATA OR DROPPING TABLE.
 USE NORTHWND
 GO
---1.      Create a view named °∞view_product_order_[your_last_name]°±, list all products and total ordered quantity for that product.
+--1.      Create a view named ‚Äúview_product_order_[your_last_name]‚Äù, list all products and total ordered quantity for that product.
 CREATE VIEW view_product_order_Qiu AS
 SELECT p.ProductID, ProductName, COUNT(o.Quantity) 'Ordered Quantity' 
 FROM Products p JOIN [Order Details] o ON p.ProductID = O.ProductID
@@ -10,7 +10,7 @@ GROUP BY p.ProductName, p.ProductID
 --SELECT *
 --FROM view_product_order_Qiu
 
---2.      Create a stored procedure °∞sp_product_order_quantity_[your_last_name]°± 
+--2.      Create a stored procedure ‚Äúsp_product_order_quantity_[your_last_name]‚Äù 
 --that accept product id as an input and total quantities of order as output parameter.
 CREATE PROC sp_product_order_quantity_Qiu
 @id int,
@@ -27,7 +27,7 @@ END
 --exec sp_product_order_quantity_Qiu 3, @total out
 --print @total
 
---3.      Create a stored procedure °∞sp_product_order_city_[your_last_name]°± that accept product 
+--3.      Create a stored procedure ‚Äúsp_product_order_city_[your_last_name]‚Äù that accept product 
 --name as an input and top 5 cities that ordered most that product combined with the total quantity 
 --of that product ordered from that city as output.***
 
@@ -49,11 +49,11 @@ BEGIN
 	HAVING @pname = p.ProductName
 END
 
---4.      Create 2 new tables °∞people_your_last_name°± °∞city_your_last_name°±. 
+--4.      Create 2 new tables ‚Äúpeople_your_last_name‚Äù ‚Äúcity_your_last_name‚Äù. 
 --City table has two records: {Id:1, City: Seattle}, {Id:2, City: Green Bay}. 
 --People has three records: {id:1, Name: Aaron Rodgers, City: 2}, {id:2, Name: Russell Wilson, City:1}, 
 --{Id: 3, Name: Jody Nelson, City:2}. Remove city of Seattle. If there was anyone from Seattle, 
---put them into a new city °∞Madison°±. Create a view °∞Packers_your_name°± lists all people from Green Bay. 
+--put them into a new city ‚ÄúMadison‚Äù. Create a view ‚ÄúPackers_your_name‚Äù lists all people from Green Bay. 
 --If any error occurred, no changes should be made to DB. (after test) Drop both tables and view.
 CREATE TABLE people_Qiu(id int, name char(20), cityid int)
 CREATE TABLE city_Qiu(cityid int, city char(20))
@@ -75,13 +75,10 @@ DROP TABLE city_Qiu
 DROP VIEW Packers_Qiu
 
 
---5.       Create a stored procedure °∞sp_birthday_employees_[you_last_name]°± that 
---creates a new table °∞birthday_employees_your_last_name°± and fill it with all employees 
+--5.       Create a stored procedure ‚Äúsp_birthday_employees_[you_last_name]‚Äù that 
+--creates a new table ‚Äúbirthday_employees_your_last_name‚Äù and fill it with all employees 
 --that have a birthday on Feb. (Make a screen shot) drop the table. Employee table should not be affected.
 CREATE TABLE birthday_employees_Qiu(name char(20), birthdate datetime)
-
-Select BirthDate
-from Employees
 
 CREATE PROCEDURE sp_birthday_employees_Qiu
 (@bd datetime)
@@ -92,6 +89,12 @@ SELECT FirstName + ' ' + LastName AS Name, BirthDate
 FROM Employees
 WHERE MONTH(BirthDate) = @bd
 END
+
+INSERT INTO birthday_employees_Qiu
+exec sp_birthday_employees_Qiu 2
+
+--SELECT *
+--FROM birthday_employees_Qiu
 
 --6.      How do you make sure two tables have the same data?
 
